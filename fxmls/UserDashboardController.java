888373a885f36eb93a1d1abe25e171a6ccc023b7
Auguste <auguste.tomaseviciute@gmail.com>
2018-12-04 16:43:57 +0000

    Added main

--- fxmls/UserDashboardController.java
+++ fxmls/UserDashboardController.java
@@ -1,4 +1,59 @@
-package fxmls;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
 public class UserDashboardController {
+
+    @FXML
+    private BorderPane userDashboard;
+
+    @FXML
+    private ImageView avatar_img;
+
+    @FXML
+    private Button myAccountButton;
+
+    @FXML
+    private Button searchResourceButton;
+
+    @FXML
+    private Button viewBorrowedResourcesButton;
+
+    @FXML
+    private Button viewRequestedResourcesButton;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    void addNewResource(MouseEvent event) {
+
+    }
+
+    @FXML
+    void loginPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void memberSearch(MouseEvent event) {
+
+    }
+
+    @FXML
+    void myAccount(MouseEvent event) {
+
+    }
+
+    @FXML
+    void searchResource(MouseEvent event) {
+
+    }
+
 }
+
+
