6681ab0b1652d0a3501913bcaf7577e9a0c741b3
KrisHaroun <44366970+KrisHaroun@users.noreply.github.com>
2018-12-05 23:44:29 +0000

    Update AvatarMain.java

--- source/tawelib/AvatarMain.java
+++ source/tawelib/AvatarMain.java
@@ -5,12 +5,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class AvatarMain extends Application {
+/**
+* This is a subclass of the superclass Application
+* <br>
+* It is a main class that launches the avatar application
+*
+*
+* @author Sebastian Gappa
+* @version 1.0
+* @since 05/12/2018
+*/
 
+public class AvatarMain extends Application {
+    
+    /**
+    * This start method creates a new scene by loading the "Avatar.fxlml" file
+    */
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("tawelib/Avatar.fxml"))));
         stage.show();
     }
+    
+    /**
+    * This is the main method that launches the application
+    */
 
     public static void main(String[] args) {
 
