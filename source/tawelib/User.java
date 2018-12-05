b7af873338118650b884e4aa2cae22020b585e88
KrisHaroun <44366970+KrisHaroun@users.noreply.github.com>
2018-12-05 22:53:42 +0000

    Update User.java

--- source/tawelib/User.java
+++ source/tawelib/User.java
@@ -4,7 +4,7 @@
  * It stores information about a User
  *
  *
- * @author Ronalyn Lilyanne
+ * @author Kristofas Haroun
  * @version 1.0
  * @since 05/12/2018
  */
@@ -17,6 +17,16 @@ public class User {
 	protected String phoneNum;
 	protected Double balance;
 	protected String avatarID;
+	
+/**
+* This is a User constructor that initialises an instance of a user
+* @param userName - the username of a user
+* @param firstName - the first name of a user
+* @param lastName - the last name of a user
+* @param phoneNum - the phone number of a user
+* @param balance - the balance of a user
+* @param avatarID - the ID of the avatar of a user
+*/
 
 	public User(String userName, String firstName, String lastName, String phoneNum, Double balance, String avatarID) {
 
@@ -27,42 +37,92 @@ public class User {
 		this.balance = balance;
 		this.avatarID = avatarID;
 	}
+	
+	/**
+	* set method for username
+	* @param userName
+	*/
 
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	
+	/**
+	* get method for username
+	* @return userName
+	*/
 
 	public String getUserName() {
 		return userName;
 	}
+	
+	/**
+	* set method for first name
+	* @param firstName
+	*/
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
+	/**
+	* get method for username
+	* @return userName
+	*/
 
 	public String getFirstName() {
 		return firstName;
 	}
+	
+	/**
+	* set method for last name
+	* @param lastName
+	*/
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
+	/**
+	* get method for username
+	* @return userName
+	*/
 
 	public String getLastName() {
 		return lastName;
 	}
+	
+	/**
+	* set method for phone number
+	* @param phoneNum
+	*/
 
 	public void setPhone(String phoneNum) {
 		this.phoneNum = phoneNum;
 	}
+	
+	/**
+	* get method for username
+	* @return userName
+	*/
 
 	public String getPhone() {
 		return phoneNum;
 	}
+	
+	/**
+	* set method for avatar
+	* @param avatarID
+	*/
 
 	public void setAvatarID(String avatarID) {
 		this.avatarID = avatarID;
 	}
+	
+	/**
+	* get method for username
+	* @return userName
+	*/
 
 	public String getAvatarID() {
 		return avatarID;
