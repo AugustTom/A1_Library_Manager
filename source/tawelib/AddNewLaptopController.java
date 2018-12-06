c1eaf50501e9fe01067ceb257db9118d1b3ef775
KrisHaroun <44366970+KrisHaroun@users.noreply.github.com>
2018-12-06 18:34:10 +0000

    Update AddNewLaptopController.java

--- source/tawelib/AddNewLaptopController.java
+++ source/tawelib/AddNewLaptopController.java
@@ -10,6 +10,15 @@ import javafx.scene.layout.Pane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * This AddNewLaptopController Class pairs with the "AddNewLaptop.fxml" file
+ *
+ *
+ * @author Auguste Tomaseviciute
+ * @version 1.0
+ * @since 04/12/2018
+ */
+
 public class AddNewLaptopController implements Initializable {
 
     @FXML
