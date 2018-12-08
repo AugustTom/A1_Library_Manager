02860812255bef7813a3aa0e185dd3c21fca5e61
Lyn <877078@swansea.ac.uk>
2018-12-08 15:36:19 +0000

    imports for alert box

--- AddNewDVDController.java
+++ AddNewDVDController.java
@@ -1,9 +1,12 @@
 package tawelib;
 
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
