651525792c40ed7d6b8425297e5f810e6d9d97af
Samhurf <44161448+Samhurf@users.noreply.github.com>
2018-12-09 19:18:41 +0000

    Update AvatarMain.java

--- AvatarMain.java
+++ AvatarMain.java
@@ -4,12 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-   /* This start method shows the stage and creates a new scene by loading the "Avatar.fxlml" file
-    * @param stage
+ /** The main method class paired with "AvatarController"
+    * @author Seb Gappa
+    * @version 1.0
+    * @since 04/12/2018
     */
+  
 
 public class AvatarMain extends Application {
+    /** This start method shows the stage and creates a new scene by loading the "Avatar.fxlml" file
+    * @param stage
+    */
 
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Avatar.fxml"))));
