fd9b42c6f41536ef5f8f95945b5e8718717265b5
Samhurf <44161448+Samhurf@users.noreply.github.com>
2018-12-09 19:39:08 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+  /**
+ * The main class for the program.
+ * @author Ronalyn Lilyanne, Auguste Tomaseviciute
+ * @version 1.0
+ * @since 04/12/2018
+ */
+
 public class Main extends Application {
     public static Stage parentStage;
 
+    /**
+    *@throws Exception
+    */
     @Override
     public void start(Stage stage) throws Exception{
         parentStage = stage;
