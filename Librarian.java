b8dbb9cfe4f935416f64a7735d8e2024833abe79
KrisHaroun <44366970+KrisHaroun@users.noreply.github.com>
2018-12-09 16:45:00 +0000

    Update Librarian.java

--- Librarian.java
+++ Librarian.java
@@ -1,5 +1,16 @@
 package tawelib;
 
+/* This class is a subclass of the User superclass
+ * <br>
+ * It stores information about a Librarian
+ *
+ *
+ * @author Sam Hurford
+ * @author Kristofas Haroun
+ * @version 1.0
+ * @since 05/12/2018
+ */
+
 public class Librarian extends User {
 
     private String employDate;
@@ -14,18 +25,38 @@ public class Librarian extends User {
         this.staffNum = staffNum;
 
     }
+    
+    /**
+	* get method for employment date
+	* @return employDate
+	*/
 
     public String getEmployDate() {
         return employDate;
     }
+    
+    /**
+	* set method for employment date
+	* @param employDate
+	*/
 
     public void setEmployDate(String employDate) {
         this.employDate = employDate;
     }
+    
+    /**
+	* get method for staff number
+	* @return staffNum
+	*/
 
     public int getStaffNum() {
         return staffNum;
     }
+    
+    /**
+	* set method for staff number
+	* @param staffNum
+	*/
 
     public void setStaffNum(int staffNum) {
         this.staffNum = staffNum;
