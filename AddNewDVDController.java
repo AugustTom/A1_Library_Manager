e8d1df9928af8cf02411cb07d41b4ff2f0c4a7c7
KrisHaroun <44366970+KrisHaroun@users.noreply.github.com>
2018-12-09 21:24:49 +0000

    Update AddNewDVDController.java

--- AddNewDVDController.java
+++ AddNewDVDController.java
@@ -50,15 +50,8 @@ public class AddNewDVDController extends AddNewSuperclassController{
     /**
      * This method adds a new DVD to the database
      * <br>
-     * It also has a contructor that initialises a DVD object
-     * @param dvdID - ID of a DVD
-     * @param dvdTitle - title of a DVD
-     * @param dvdYear - year in which the DVD was released
-     * @param IDsOfCopies - IDs of DVD copies
-     * @param dvdDirector - director of a DVD
-     * @param dvdRuntime - runtime of a DVD
-     * @param dvdSubtitleLanguages - languages of the subtitles of a DVD
-     * @param dvdLanguage - language of a DVD
+     * It contains a contructor that initialises a DVD object
+     * @param event
      */
 
     @FXML
